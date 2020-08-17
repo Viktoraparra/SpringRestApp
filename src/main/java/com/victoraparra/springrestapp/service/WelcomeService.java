44e4c364741d498016d2cd1651c4b1b8d2de521d
Viktoraparra <victoraparrapineda@gmail.com>
2020-08-17 19:49:40 -0300

    Adding Yaml for more readable

--- src/main/java/com/victoraparra/springrestapp/service/WelcomeService.java
+++ src/main/java/com/victoraparra/springrestapp/service/WelcomeService.java
@@ -1,12 +1,16 @@
 package com.victoraparra.springrestapp.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 // Spring to manage this bean and create a instance of this
 @Component
 public class WelcomeService {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
     public String retrieveWelcomeMessage(){
         // Complex
-        return "Good morning updated!";
+        return welcomeMessage;
     }
 }
